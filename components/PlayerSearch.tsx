c3bc6be7ecdfb62fe9054b3353941f138bc2321c
JimLiu0 <jim.liu.95@gmail.com>
2025-06-29 10:42:40 -0700

    Fix apos bug

--- components/PlayerSearch.tsx
+++ components/PlayerSearch.tsx
@@ -143,7 +143,7 @@ export default function PlayerSearch() {
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search text-gray-400" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
           </svg>
-          <span className="text-gray-400">Search for a player's stats</span>
+          <span className="text-gray-400">Search for a player&apos;s stats</span>
         </button>
       )}
       
