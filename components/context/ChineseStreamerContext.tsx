e595008c11965918d3fe9c42a977968fd0705dcb
JimLiu0 <jim.liu.95@gmail.com>
2025-11-15 10:10:49 -0800

    Remove unused components

--- components/context/ChineseStreamerContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const ChineseStreamerContext = createContext<Record<string, string>>({});
-
-export const useChineseStreamers = () => useContext(ChineseStreamerContext);
